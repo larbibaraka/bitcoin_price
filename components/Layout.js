4d7433d9fcac9362e9af26b1c589a15cd1d8e3c3
larbibaraka <laridev@gmail.com>
2019-01-05 21:56:42 +0100

    adding the ability to change themes from bootswatch

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,40 @@
 import Navbar from "./Navbar";
 import Head from 'next/head';
-const Layout = (props) => (
-   <div>
-      <Head>
-         <title>BitCoin Price</title>
-         <link rel="stylesheet" 
-            href="https://bootswatch.com/4/flatly/bootstrap.min.css"
-         />  
-      </Head>
-      <Navbar/> 
-      <div className="container">{props.children}</div>
-   </div> 
+class Layout extends React.Component {
+   
+   constructor(props){
+      super(props);
+      this.state = {
+         currentTheme : 'https://bootswatch.com/4/flatly/bootstrap.min.css'
+        
+      }
+   }
 
-    
+   ChangeTheme = (value) =>{
+         this.setState({
+            currentTheme : `https://bootswatch.com/4/${value}/bootstrap.min.css`
+         })
+   }
+   
+   
+   render(){
+      return (
+         <div>
+         <Head>
+            <title>BitCoin Price</title>
+            <link rel="stylesheet" 
+               href={this.state.currentTheme}
+            />  
+         </Head>
+         <Navbar ChangeTheme={this.ChangeTheme}/> 
+         <div className="container">{this.props.children}</div>
+      </div> 
+      )
+   }
+
+}
 
 
-);
+ 
 
 export default Layout;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,67 @@
 import Link from 'next/link';
-const Navbar = () => (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-         <a className="navbar-brand" href="#">BitCoin Price</a>
-         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-         </button>
+class Navbar extends React.Component {
 
-         <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav mr-auto">
-               <li className="nav-item active">
-                  <Link href="/"><a className="nav-link">Home <span className="sr-only">(current)</span></a></Link>
-               </li>
-               <li className="nav-item">
-                  <Link href="/about"><a className="nav-link" >About</a></Link>
-               </li>
-            </ul>
-          </div>
-         </nav>
-   </div>
+   constructor(props){
+      super(props);
+   }
+
+   onChange = (e) =>{
+      const value = e.target.value;
+      this.props.ChangeTheme(value);
+      
+   }
+
+
+   render(){
+      return(
+         <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                  <a className="navbar-brand" href="#">BitCoin Price</a>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                     <span className="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div className="collapse navbar-collapse" id="navbarColor01">
+                     <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                           <Link href="/"><a className="nav-link">Home <span className="sr-only">(current)</span></a></Link>
+                        </li>
+                        <li className="nav-item">
+                           <Link href="/about"><a className="nav-link" >About</a></Link>
+                        </li>
+                        <li>
+                           <select className="form-control float-right" onChange={this.onChange.bind(this)}>
+                                 <option>-select a theme-</option>
+                                 <option value="cerulean">Cerulean</option>
+                                 <option value="cosmo">Cosmo</option>
+                                 <option value="cyborg">Cyborg</option>
+                                 <option value="darkly">Darkly</option>
+                                 <option value="flatly">Flatly</option>
+                                 <option value="journal">Journal</option>
+                                 <option value="litera">Litera</option>
+                                 <option value="lumen">Lumen</option>
+                                 <option value="lux">Lux</option>
+                                 <option value="materia">Materia</option>
+                                 <option value="pulse">Pulse</option>
+                                 <option value="sandstone">Sandstone</option>
+                                 <option value="simplex">Simplex</option>
+                                 <option value="sketchy">Sketchy</option>
+                                 <option value="slate">Slate</option>
+                                 <option value="solar">Solar</option>
+                                 <option value="spacelab">Spacelab</option>
+                                 <option value="superhero">Superhero</option>
+                                 <option value="united">United</option>
+                                 <option value="yeti">Yeti</option>
+                           </select>
+                        </li>
+
+                     </ul>
+                  </div>
+                  </nav>
+            </div>
    
+      )
+   }
 
-
-)
-
+}
 export default Navbar;
