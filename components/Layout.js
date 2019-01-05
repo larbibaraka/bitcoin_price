ebb6d0cd00ebe37813afb1461921f91e1dd7d0ea
larbibaraka <laridev@gmail.com>
2019-01-05 19:04:21 +0100

    bring bootstrap to the game :D

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
 import Navbar from "./Navbar";
-
+import Head from 'next/head';
 const Layout = (props) => (
    <div>
+      <Head>
+         <title>BitCoin Price</title>
+         <link rel="stylesheet" 
+            href="https://bootswatch.com/4/solar/bootstrap.min.css"
+         />  
+      </Head>
       <Navbar/> 
       {props.children}
    </div> 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,27 @@
 import Link from 'next/link';
 const Navbar = () => (
    <div>
-      <ul>
-            <li><Link href="/"><a>Home</a></Link></li>
-            <li><Link href="/about"><a>About</a></Link></li>
-      </ul>
+     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+         <a className="navbar-brand" href="#">BitCoin Price</a>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+         </button>
+
+         <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+               <li className="nav-item active">
+                  <Link href="/"><a className="nav-link">Home <span className="sr-only">(current)</span></a></Link>
+               </li>
+               <li className="nav-item">
+                  <Link href="/about"><a className="nav-link" >About</a></Link>
+               </li>
+            </ul>
+          </div>
+         </nav>
    </div>
+   
+
+
 )
 
 export default Navbar;
